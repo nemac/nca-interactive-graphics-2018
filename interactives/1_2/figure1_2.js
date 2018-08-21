9062923c2cc8e162461b3b0380e5ed70c49f73c4
John Frimmel <jrfrimme@unca.edu>
2018-08-21 13:19:36 -0400

    adds first prototype of figure 1_2

---
+++ interactives/1_2/figure1_2.js
@@ -0,0 +1,42 @@
+(function () {
+    "use strict";
+
+    function handleOverlayClick(e) {
+        e.preventDefault();
+
+        var url = this.getAttribute("data-for");
+        var overlay = this.parentElement.parentElement.getElementsByClassName("image-clickable-overlay")[0];
+        console.log(overlay);
+        var overlayImage = overlay.getElementsByClassName("image-clickable-overlay-image")[0];
+        overlayImage.setAttribute("src", url);
+        overlayImage.setAttribute("title", this.getAttribute("title"));
+        overlay.classList.add("active");
+    }
+
+    function handleOverlayDismissClick(e) {
+        e.preventDefault();
+
+        this.parentElement.classList.remove("active");
+    }
+
+    function bindImageClickableEvents() {
+        var i, l;
+        var elements = document.getElementsByClassName("image-clickable-main-overlay-link");
+
+        for (i = 0, l = elements.length; i < l; i += 1) {
+            elements[i].addEventListener("click", handleOverlayClick);
+        }
+    }
+
+    function bindImageClickableDismissEvents() {
+        var i, l;
+        var elements = document.getElementsByClassName("image-clickable-overlay-dismiss");
+
+        for (i = 0, l = elements.length; i < l; i += 1) {
+            elements[i].addEventListener("click", handleOverlayDismissClick);
+        }
+    }
+
+    bindImageClickableEvents();
+    bindImageClickableDismissEvents();
+})();

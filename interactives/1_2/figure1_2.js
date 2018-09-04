19106a938cd17ef365011d2e0d800dd4ad96d4e7
John Frimmel <jrfrimme@unca.edu>
2018-09-04 16:59:09 -0400

    adds overlay arrows

--- interactives/1_2/figure1_2.js
+++ interactives/1_2/figure1_2.js
@@ -14,19 +14,91 @@
         return elem.querySelector(".image-clickable-overlay");
     }
 
-    function getOverlayImage(elem) {
-        return elem.querySelector(".image-clickable-overlay-image");
+    function getOverlayImage(elem, index) {
+        return elem.querySelectorAll(".image-clickable-overlay-image")[index];
+    }
+
+    function getActiveOverlayImage(elem) {
+        return elem.querySelector(".image-clickable-overlay-image.active-image");
+    }
+
+    function getArrowLeft(elem) {
+        return elem.querySelector(".image-clickable-overlay-arrow-left");
+    }
+
+    function getArrowRight(elem) {
+        return elem.querySelector(".image-clickable-overlay-arrow-right");
+    }
+
+    function enableArrow(arrow) {
+        arrow.classList.remove("inactive");
+    }
+
+    function disableArrow(arrow) {
+        arrow.classList.add("inactive");
+    }
+
+    function disableArrowLeft(wrapper) {
+        disableArrow(getArrowLeft(wrapper));
+    }
+
+    function disableArrowRight(wrapper) {
+        disableArrow(getArrowRight(wrapper));
+    }
+
+    function enableArrows(wrapper) {
+        enableArrow(getArrowLeft(wrapper));
+        enableArrow(getArrowRight(wrapper));
+    }
+
+    function disableArrows(activeImage, wrapper) {
+        if (!activeImage.previousElementSibling) {
+            disableArrowLeft(wrapper);
+        }
+        if (!activeImage.nextElementSibling) {
+            disableArrowRight(wrapper);
+        }
+    }
+
+    function handleArrows(activeImage, wrapper) {
+        enableArrows(wrapper); // Clear any previous state
+        disableArrows(activeImage, wrapper); // Disable if neccesary
     }
 
     function loadOverlayImage(elem) {
-        var url = elem.getAttribute("data-for");
+        var index = elem.getAttribute("data-for");
         var wrapper = getClickableWrapper(elem);
         var overlay = getOverlayWrapper(wrapper);
-        var overlayImage = getOverlayImage(wrapper);
+        var overlayImage = getOverlayImage(wrapper, index);
 
-        overlayImage.setAttribute("src", url);
-        overlayImage.setAttribute("title", elem.getAttribute("title"));
+        var activeImage = getActiveOverlayImage(wrapper);
+        if (activeImage) {
+            activeImage.classList.remove("active-image");
+        }
+
+        overlayImage.classList.add("active-image");
         overlay.classList.add("active");
+
+        handleArrows(overlayImage, wrapper);
+    }
+
+    function switchOverlayImage(elem, getter) {
+        var wrapper = getClickableWrapper(elem);
+        var activeImage = getActiveOverlayImage(wrapper);
+        var newActiveImage = activeImage[getter];
+        if (newActiveImage) {
+            activeImage.classList.remove("active-image");
+            newActiveImage.classList.add("active-image");
+            handleArrows(newActiveImage, wrapper);
+        }
+    }
+
+    function switchOverlayImageRight(elem) {
+        switchOverlayImage(elem, "nextElementSibling");
+    }
+
+    function switchOverlayImageLeft(elem) {
+        switchOverlayImage(elem, "previousElementSibling");
     }
 
     function hideClickableOverlay(elem) {
@@ -39,10 +111,37 @@
         loadOverlayImage(this);
     }
 
+    function handleArrowClickRight(e) {
+        e.preventDefault();
+        switchOverlayImageRight(this);
+    }
+
+    function handleArrowClickLeft(e) {
+        e.preventDefault();
+        switchOverlayImageLeft(this);
+    }
+
     function handleClickableLinkKeypress(e) {
         if (e.key.toLowerCase() === "enter") {
             e.preventDefault();
             loadOverlayImage(this);
+        }
+    }
+
+    function handleArrowKeypress(e) {
+        if (!getOverlayWrapper(this).classList.contains("active")) {
+            return;
+        }
+
+        var key = e.key.toLowerCase();
+        if (key === "arrowleft" || key === "left" || key === "arrowright" || key === "right") {
+            e.preventDefault();
+        }
+        if (key === "arrowleft" || key === "left") {
+            switchOverlayImageLeft(this);
+        }
+        if (key === "arrowright" || key === "right") {
+            switchOverlayImageRight(this);
         }
     }
 
@@ -61,6 +160,23 @@
         }
     }
 
+    function bindArrowEvents() {
+        var i, l;
+        var rightArrows = document.getElementsByClassName("image-clickable-overlay-arrow-right");
+        var leftArrows = document.getElementsByClassName("image-clickable-overlay-arrow-left");
+        var imageClickables = document.getElementsByClassName("image-clickable");
+
+        for (i = 0, l = rightArrows.length; i < l; i += 1) {
+            rightArrows[i].addEventListener("click", handleArrowClickRight);
+        }
+        for (i = 0, l = leftArrows.length; i < l; i += 1) {
+            leftArrows[i].addEventListener("click", handleArrowClickLeft);
+        }
+        for (i = 0, l = imageClickables.length; i < l; i += 1) {
+            imageClickables[i].addEventListener("keydown", handleArrowKeypress);
+        }
+    }
+
     function bindImageClickableDismissEvents() {
         var i, l;
         var elements = document.getElementsByClassName("image-clickable-overlay-dismiss");
@@ -72,4 +188,29 @@
 
     bindImageClickableEvents();
     bindImageClickableDismissEvents();
+    bindArrowEvents();
 })();
+
+// Source: https://github.com/jserz/js_piece/blob/master/DOM/NonDocumentTypeChildNode/nextElementSibling/nextElementSibling.md
+// Polyfill for IE9 & Safari
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('nextElementSibling')) {
+      return;
+    }
+    Object.defineProperty(item, 'nextElementSibling', {
+      configurable: true,
+      enumerable: true,
+      get: function () {
+        var el = this;
+        while (el = el.nextSibling) {
+          if (el.nodeType === 1) {
+              return el;
+          }
+        }
+        return null;
+      },
+      set: undefined
+    });
+  });
+})([Element.prototype, CharacterData.prototype]);

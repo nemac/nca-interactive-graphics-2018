95664544bd23c2ec47114e0b125174a9f4408605
John Frimmel <jrfrimme@unca.edu>
2018-10-05 10:35:22 -0400

    adds basic interactions to 2.1

---
+++ interactives/2_1/figure2_1.js
@@ -0,0 +1,84 @@
+(function () {
+    "use strict";
+
+    function getGraphHideClass() {
+        return "graph-line--inactive";
+    }
+
+    function getWrapper(elem) {
+        return elem.closest(".graph-selectable");
+    }
+
+    function getGraphLine(key, wrapper) {
+        return wrapper.querySelectorAll(".graph-line--" + key);
+    }
+
+    function showGraphLine(line) {
+        line.classList.remove(getGraphHideClass());
+    }
+
+    function showGraphLines(lines) {
+        lines.forEach(showGraphLine);
+    }
+
+    function hideGraphLine(line) {
+        line.classList.add(getGraphHideClass());
+    }
+
+    function hideGraphLines(lines) {
+        lines.forEach(hideGraphLine);
+    }
+
+    function enableGraphLine(key, wrapper) {
+        showGraphLines(getGraphLine(key, wrapper));
+    }
+
+    function disableGraphLine(key, wrapper) {
+        hideGraphLines(getGraphLine(key, wrapper));
+    }
+
+    function toggleGraph(key, enable, wrapper) {
+        enable ? enableGraphLine(key, wrapper) : disableGraphLine(key, wrapper);
+    }
+
+    function handleGraphLineChange(elem) {
+        toggleGraph(elem.getAttribute("data-for"), elem.checked, getWrapper(elem));
+    }
+
+    function handleCheckboxChange(e) {
+        e.preventDefault();
+        handleGraphLineChange(this);
+    }
+
+    function bindCheckboxChange(elem) {
+        elem.addEventListener("change", handleCheckboxChange);
+    }
+
+    (function bindCheckboxChanges() {
+        var checkboxes = document.getElementsByClassName("graph-checkbox");
+        Array.prototype.forEach.call(checkboxes, bindCheckboxChange);
+    })();
+})();
+
+// Polyfill for Element.closest for IE9+
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/closest#Polyfill
+if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector || 
+                                Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+    Element.prototype.closest = function(s) {
+        var el = this;
+        if (!document.documentElement.contains(el)) {
+            return null;
+        }
+        do {
+            if (el.matches(s)) {
+                return el;
+            }
+            el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1); 
+        return null;
+    };
+}

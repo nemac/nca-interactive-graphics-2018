8a36f09aefed96de35728d3905703ffa5de1bd47
John Frimmel <jrfrimme@unca.edu>
2018-10-25 14:32:16 -0400

    cleans up 14.3

--- interactives/14_3/figure14_3.js
+++ interactives/14_3/figure14_3.js
@@ -53,7 +53,7 @@
         return container;
     }
 
-    // returns the overall config object for the custom leaflet zoom buttons
+    // Returns the overall config object for the custom leaflet zoom buttons
     function makeZoomButtonConfig() {
         return {
             options: {
@@ -66,17 +66,8 @@
         }
     }
 
-    // creates the basemap
-    var basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 10,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox.streets'
-    });
-
-    var mapContainer = document.getElementsByClassName("figure14_3")[0]
-
+    // Gets the fill color for each category of storm. Fall through on the switch statement is
+    // deliberate.
     function getStormLayerColor(category) {
         switch (category) {
             case 1:
@@ -97,6 +88,7 @@
         }
     }
 
+    // Configures the storm category layers
     function makeStormLayerConfig(category) {
         return {
             style: function(feature) {
@@ -111,6 +103,7 @@
         };
     }
 
+    // Gets the fill color for the hospitals affected by each category of storm.
     function getHospitalColor(param) {
         switch (param) {
             case 1:
@@ -128,14 +121,18 @@
         }
     }
 
+    // Handles the conversion of a word to title case
     function titleCaseHandler(word) {
         return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
     }
 
+    // The hospitals geojson text is all uppercase, so we need to reprocess it to title case (i.e.
+    // every word has its first letter capitalized) so that it is more easily readable.
     function convertToTitleCase(string) {
         return string.replace(/\w\S*/g, titleCaseHandler)
     }
 
+    // Creates description of hospital flooding
     function makeHospitalFloodingString(category) {
         return (category !== null) ? "Category " + category + " hurricanes and above" :
             "No categories of hurricanes";
@@ -143,14 +140,16 @@
 
     // returns string that contains the content for each popup
     function makePopup (properties) {
-        return "<div><strong><a href='" + properties.WEBSITE + "' target='_blank'>" + convertToTitleCase(properties.NAME) + "</a></strong></div>"
+        return "<div><strong>" + convertToTitleCase(properties.NAME) + "</strong></div>"
             + "<div><strong>Type of Services</strong>: " + convertToTitleCase(properties.TYPE) + "</div>"
             + "<div><strong>Projected to be flooded by</strong>: " + makeHospitalFloodingString(properties.Low_Cat) + "</div>";
     }
 
+    // Configures the hospital layer
     function makeHospitalLayerConfig() {
         return {
             pointToLayer: function (feature, latlng) {
+                var popup = L.popup().setContent(makePopup(feature.properties));
                 return L.circleMarker(latlng, {
                     fillColor: getHospitalColor(feature.properties.Low_Cat),
                     radius: 8,
@@ -158,7 +157,7 @@
                     weight: 1,
                     opacity: 1,
                     fillOpacity: 0.9
-                }).bindPopup(makePopup(feature.properties));
+                }).bindPopup(popup);
             },
 
             style: function(feature) {
@@ -169,61 +168,82 @@
         };
     }
 
-    // creates the maps and adds all layers to it.
-    var mymap = L.map(mapContainer, {
-        layers: [basemap],
-        minZoom: 7,
-        maxZoom: 18
-    }).setView([32.77572, -79.90775], 9);
+    // Creates leaflet panes for each of the layers. These allow them to always be in the correct
+    // order when toggled on and off.
+    function makePanes(map) {
+        map.createPane("Hospitals");
+        map.createPane("Category 1");
+        map.createPane("Category 2");
+        map.createPane("Category 3");
+        map.createPane("Category 4");
+        map.createPane("Category 5");
 
-    mymap.createPane("Category 1");
-    mymap.createPane("Category 2");
-    mymap.createPane("Category 3");
-    mymap.createPane("Category 4");
-    mymap.createPane("Category 5");
-    mymap.createPane("Hospitals");
+        map.getPane("Hospitals").style.zIndex = 460;
+        map.getPane("Category 1").style.zIndex = 450;
+        map.getPane("Category 2").style.zIndex = 440;
+        map.getPane("Category 3").style.zIndex = 430;
+        map.getPane("Category 4").style.zIndex = 420;
+        map.getPane("Category 5").style.zIndex = 410;
+    }
 
-    mymap.getPane("Hospitals").style.zIndex = 460;
-    mymap.getPane("Category 1").style.zIndex = 450;
-    mymap.getPane("Category 2").style.zIndex = 440;
-    mymap.getPane("Category 3").style.zIndex = 430;
-    mymap.getPane("Category 4").style.zIndex = 420;
-    mymap.getPane("Category 5").style.zIndex = 410;
+    // Initializes layers and adds them to the map
+    function createLayers(map) {
+        var stormLayers = {
+            "Category 1": new L.GeoJSON.AJAX('../../interactives/14_3/geojson2/cat1.geojson', makeStormLayerConfig("Category 1")),
+            "Category 2": new L.GeoJSON.AJAX('../../interactives/14_3/geojson2/cat2.geojson', makeStormLayerConfig("Category 2")),
+            "Category 3": new L.GeoJSON.AJAX('../../interactives/14_3/geojson2/cat3.geojson', makeStormLayerConfig("Category 3")),
+            "Category 4": new L.GeoJSON.AJAX('../../interactives/14_3/geojson2/cat4.geojson', makeStormLayerConfig("Category 4")),
+            "Category 5": new L.GeoJSON.AJAX('../../interactives/14_3/geojson2/cat5.geojson', makeStormLayerConfig("Category 5")),
+            "Hospitals": new L.GeoJSON.AJAX("../../interactives/14_3/geojson2/hospitals.geojson", makeHospitalLayerConfig())
+        };
 
-    var stormLayers = {
-        "Category 1": new L.GeoJSON.AJAX('../../interactives/14_3/geojson2/cat1.geojson', makeStormLayerConfig("Category 1")),
-        "Category 2": new L.GeoJSON.AJAX('../../interactives/14_3/geojson2/cat2.geojson', makeStormLayerConfig("Category 2")),
-        "Category 3": new L.GeoJSON.AJAX('../../interactives/14_3/geojson2/cat3.geojson', makeStormLayerConfig("Category 3")),
-        "Category 4": new L.GeoJSON.AJAX('../../interactives/14_3/geojson2/cat4.geojson', makeStormLayerConfig("Category 4")),
-        "Category 5": new L.GeoJSON.AJAX('../../interactives/14_3/geojson2/cat5.geojson', makeStormLayerConfig("Category 5")),
-        "Hospitals": new L.GeoJSON.AJAX("../../interactives/14_3/geojson2/hospitals.geojson", makeHospitalLayerConfig())
+        map.addLayer(stormLayers["Category 1"])
+            .addLayer(stormLayers["Category 2"])
+            .addLayer(stormLayers["Category 3"])
+            .addLayer(stormLayers["Category 4"])
+            .addLayer(stormLayers["Category 5"])
+            .addLayer(stormLayers["Hospitals"]);
 
-//        "Category 1 Topo": new L.TopoJSON(cat1, makeStormLayerConfig("Category 1")),
-//        "Category 2": new L.TopoJSON(cat2, makeStormLayerConfig("Category 2")),
-//        "Category 3": new L.TopoJSON(cat3, makeStormLayerConfig("Category 3")),
-//        "Category 4": new L.TopoJSON(cat4, makeStormLayerConfig("Category 4")),
-//        "Category 5": new L.TopoJSON(cat5, makeStormLayerConfig("Category 5"))
-    };
+        return stormLayers;
+    }
 
-    mymap.addLayer(stormLayers["Category 1"])
-        .addLayer(stormLayers["Category 2"])
-        .addLayer(stormLayers["Category 3"])
-        .addLayer(stormLayers["Category 4"])
-        .addLayer(stormLayers["Category 5"])
-        .addLayer(stormLayers["Hospitals"]);
+    // creates the basemap
+    function makeBasemap() {
+        return L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 10,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: 'mapbox.streets'
+        });
+    }
 
-    // adds filterable controls for the three types of layers to the map
-    L.control.layers({}, stormLayers, { collapsed: false }).addTo(mymap);
+    // Creates figure 14.3 in a container element
+    function makeMap(container) {
+        var basemap = makeBasemap();
 
-//mymap.on('moveend', function() { 
-//     console.log(mymap.getCenter());
-//     console.log(mymap.getZoom());
-//     console.log(mymap.getBounds());
-//});
+        // creates the map
+        var mymap = L.map(container, {
+            layers: [basemap],
+            minZoom: 7,
+            maxZoom: 18
+        }).setView([32.77572, -79.90775], 9);
 
+        makePanes(mymap);
+        var stormLayers = createLayers(mymap);
 
-//T {_southWest: M, _northEast: M}_northEast: M {lat: 33.53890866167807, lng: -78.90970945358278}_southWest: M {lat: 32.15448738970588, lng: -80.79386472702028}__proto__: Object
+        // adds filterable controls for the three types of layers to the map
+        L.control.layers({}, stormLayers, { collapsed: false }).addTo(mymap);
 
-    var zoomButtons = L.Control.extend(makeZoomButtonConfig());
-    mymap.addControl(new zoomButtons());
+        var zoomButtons = L.Control.extend(makeZoomButtonConfig());
+        mymap.addControl(new zoomButtons());
+    }
+
+    (function handleFigure14_3() {
+        var i, l;
+        var mapContainers = document.getElementsByClassName("figure14_3");
+        for (i = 0, l = mapContainers.length; i < l; i++) {
+            makeMap(mapContainers[i]);
+        }
+    })();
 })();
